1a70d7a259682a3f23684bd7b44196e957b969ab
ianfleming464 <ianflemingdeveloper@gmail.com>
2022-08-20 22:34:42 +0200

    Began to build the MovieView

--- src/components/Movies/MovieView.jsx
+++ src/components/Movies/MovieView.jsx
@@ -1,5 +1,16 @@
+import './MovieView.css';
+
 const MovieView = ({ movie }) => {
-  return <>{movie.title}</>;
+  const { id, title, description, genre, image } = movie;
+  return (
+    <div class='d-flex'>
+      <div class='flex-shrink-0'>
+        <img src={image} alt='...'></img>
+        <h1>{title}</h1>
+      </div>
+      <div class='flex-grow-1 ms-3'>{description}</div>
+    </div>
+  );
 };
 
 export default MovieView;
